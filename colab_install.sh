e9fae81e8f5e2e57f999694e845334a62d40d38f
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2024-11-14 00:45:44 +0330

    add colab install link

---
+++ colab_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Set the branch and the NeMo directory path
+BRANCH="main"
+NEMO_DIR_PATH="NeMo"
+# Install necessary Python packages
+echo "Installing Python packages..."
+python -m pip install nemo_toolkit['all']
+python -m pip install wget
+python -m pip install sox
+python -m pip install huggingface_hub==0.23.2
+python -m pip install pytorch-lightning==2.2.1
+wget "https://raw.githubusercontent.com/saeedzou/NeMo/main/tools/ctc_segmentation/requirements.txt" -O requirements.txt
+python -m pip install -r requirements.txt
+echo "Installation completed."
