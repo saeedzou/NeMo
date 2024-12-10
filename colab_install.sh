bc7540e519c2062dc25f5d480e599c23c8bc7930
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2024-12-10 04:41:15 +0330

    add pysrt for srt support

--- colab_install.sh
+++ colab_install.sh
@@ -10,6 +10,7 @@ python -m pip install sox
 python -m pip install huggingface_hub==0.23.2
 python -m pip install pytorch-lightning==2.2.1
 python -m pip install lightning
+python -m pip install pysrt
 wget "https://raw.githubusercontent.com/saeedzou/NeMo/main/tools/ctc_segmentation/requirements.txt" -O requirements.txt
 python -m pip install -r requirements.txt
 echo "Installation completed."
