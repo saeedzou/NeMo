09f9e29282e4c4f9b6f4c71e15e46315c80fb211
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2025-01-05 01:23:59 +0330

    fix package errors

--- colab_install.sh
+++ colab_install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # Install necessary Python packages
 echo "Installing Python packages..."
-python -m pip install nemo_toolkit['asr']
-python -m pip install wget mutagen sox
-python -m pip install huggingface_hub
-python -m pip install pytorch-lightning==2.2.1
-python -m pip install lightning
+pip install git+https://github.com/NVIDIA/NeMo.git@main#egg=nemo_toolkit[asr]
+python -m pip install sox
 wget "https://raw.githubusercontent.com/saeedzou/NeMo/main/tools/ctc_segmentation/requirements.txt" -O requirements.txt
 python -m pip install -r requirements.txt
 # Clone and install ParsNorm
 git clone https://github.com/saeedzou/ParsNorm.git
 cd ParsNorm && pip install -e . && pip install -r requirements.txt
+python -m pip install hezar mutagen
 echo "Installation completed."
